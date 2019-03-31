25046d2aecfb9714cdbc900c66efc98b430c4c03
Kaluã Bentes <kaluanbentes@gmail.com>
2019-03-31 18:11:05 -0300

    Remove .DS_Store

--- atoms/address-input/stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
-
-import AddressInput from '.'
-
-storiesOf('Atoms/AddressInput', module).add('default', () => (
-  <AddressInput placeholder="Digite o seu endereço ou cep" onChange={action('Changed')} />
-))
