101c370d73510c03e43e9fc98a0a0975f92d05bd
Kaluã Bentes <kaluanbentes@gmail.com>
2019-04-20 22:30:33 -0300

    Update button styling

--- templates/base/index.js
+++ templates/base/index.js
@@ -1,9 +1,20 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Helmet from "react-helmet"
 
 import "_styles/base.css"
 
-const Base = ({ children }) => children
+const Base = ({ children }) => (
+  <>
+    <Helmet>
+      <link
+        href="https://fonts.googleapis.com/css?family=Roboto:400,500,700"
+        rel="stylesheet"
+      />
+    </Helmet>
+    {children}
+  </>
+)
 
 Base.propTypes = {
   children: PropTypes.node.isRequired,
